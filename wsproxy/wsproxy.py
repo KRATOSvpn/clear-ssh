9b5f7516378ce0a218d2a0498f32d39a2fc61626
oKRATOS <72270994+KRATOSvpn@users.noreply.github.com>
2023-08-18 05:38:15 -0400

    Update wsproxy.py

--- wsproxy/wsproxy.py
+++ wsproxy/wsproxy.py
@@ -11,7 +11,7 @@ except:
 BUFLEN = 4096 * 4
 TIMEOUT = 60
 DEFAULT_HOST = "127.0.0.1:22"
-RESPONSE = "HTTP/1.1 101 Switching Protocols \r\n\r\n"
+RESPONSE = "HTTP/1.1 200 OK \r\n\r\n"
  
 class Server(threading.Thread):
     def __init__(self, host, port):
